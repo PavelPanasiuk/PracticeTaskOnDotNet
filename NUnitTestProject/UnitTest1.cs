683771a0729d138acd7505e69ac1ec70edc22906
Pavel Panasiuk <pavel.panasiuk.at@gmail.com>
2020-12-14 18:22:44 +0300

    Remade tests.

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -10,27 +10,12 @@ namespace NUnitTestProject
         //3)Написать unit тесты для классов Calculator, StringMathCalculator, StringLingvoCalculator 
         //4)Отключить один из юнит тестов
 
-        private string _someText = "Hello world";
-        private int _actualSum;
-        private int _actualDifference;
-        private int _actualComposition;
-        private int _actualQuotient;
-        private int _actualRezultFromString;
-        private int _actualNumberFromString;
+        private string _someText = "Hello world";        
 
         [SetUp]
         public void Setup()
         {
-            Console.WriteLine(_someText);
-            CreateCalculator.Calculator calculator = new CreateCalculator.Calculator();
-            _actualSum = calculator.GetSum(2, 2);
-            _actualDifference = calculator.GetDifference(20, 11);
-            _actualComposition = calculator.GetComposition(7, 7);
-            _actualQuotient = calculator.GetQuotient(8, 4);
-            CreateCalculator.StringMathCalculator stringMath = new CreateCalculator.StringMathCalculator();
-            _actualRezultFromString = stringMath.GetSumNumbersInString("5*5");
-            CreateCalculator.StringLingvoCalculator stringLingvo = new CreateCalculator.StringLingvoCalculator();
-            _actualNumberFromString = stringLingvo.GetSumNumbersInString("hjh1h2jhn7");
+            Console.WriteLine(_someText);            
         }
 
         [Test]
@@ -40,39 +25,57 @@ namespace NUnitTestProject
         }
 
         [Test]
-        public void CheckSum()
+        public void Add_TwoNumbers_ReturnSumOfNumbers()
         {
-            Assert.AreEqual(4, _actualSum);
+            CreateCalculator.Calculator calculator = new CreateCalculator.Calculator();
+            int actualSum = calculator.GetSum(2, 2);
+            int existedSum = 4;
+            Assert.AreEqual(existedSum, actualSum);
         }
 
         [Test]
-        public void CheckDifference()
+        public void Add_TwoNumbers_ReturnDifferenceOfNumbers()
         {
-            Assert.AreEqual(9, _actualDifference);
+            CreateCalculator.Calculator calculator = new CreateCalculator.Calculator();
+            int actualDifference = calculator.GetDifference(20, 11);
+            int existedDifference = 9;
+            Assert.AreEqual(existedDifference, actualDifference);
         }
 
         [Test]
-        public void CheckComposition()
+        public void Add_TwoNumbers_ReturnCompositionOfNumbers()
         {
-            Assert.AreEqual(49, _actualComposition);
+            CreateCalculator.Calculator calculator = new CreateCalculator.Calculator();
+            int actualComposition = calculator.GetComposition(7, 7);
+            int existedComposition = 49;
+            Assert.AreEqual(existedComposition, actualComposition);
         }
 
         [Test]
-        public void CheckQuotient()
+        public void Add_TwoNumbers_ReturnQoutient()
         {
-            Assert.AreEqual(2, _actualQuotient);
+            CreateCalculator.Calculator calculator = new CreateCalculator.Calculator();
+            int actualQuotient = calculator.GetQuotient(8, 4);
+            int existedQoutient = 2;
+            Assert.AreEqual(existedQoutient, actualQuotient);
         }
 
         [Test]
-        public void CheckMathRezultFromString()
+        public void Add_StringWithTwoNumbers_ReturnRezultOfAdding()
         {
-            Assert.AreEqual(25, _actualRezultFromString);
+            CreateCalculator.StringMathCalculator stringMath = new CreateCalculator.StringMathCalculator();
+            int actualRezultFromString = stringMath.GetSumNumbersInString("5*5");
+            int existedRezult = 25;
+            Assert.AreEqual(existedRezult, actualRezultFromString);
         }
 
         [Test]
-        public void CheckNumberInString()
+        public void Add_StringWithNumbers_ReturnOnlyNumbers()
         {
-            Assert.AreEqual(127, _actualNumberFromString);
+            CreateCalculator.StringLingvoCalculator stringLingvo = new CreateCalculator.StringLingvoCalculator();
+            int actualNumberFromString = stringLingvo.GetSumNumbersInString("hjh1h2jhn7");
+            int existedNumberFromString = 127;
+            Assert.AreEqual(existedNumberFromString, actualNumberFromString);
         }
     }
 }
